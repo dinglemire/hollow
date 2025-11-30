245680cc4496a9bb754df2cd6f3be2cd5e904e27
dinglemire <enPaulius@gmail.com>
2025-11-30 21:06:41 +0200

    Update InventoryEditor.js

--- src/InventoryEditor.js
+++ src/InventoryEditor.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-// Configuration for the items we want to edit
+// Added Notches, Health, and Soul
 const INVENTORY_ITEMS = [
     { key: "geo",        name: "Geo",          img: "Geo.png",        min: 0, max: 9999999 },
+    { key: "charmSlots", name: "Notches",      img: "Notch.png",      min: 0, max: 120 }, // You can go crazy here
+    { key: "maxHealth",  name: "Masks (HP)",   img: "Mask.png",       min: 0, max: 99 },
+    { key: "MPReserveMax", name: "Soul (Max)", img: "Soul.png",       min: 0, max: 198 }, // 33 per vessel usually
     { key: "ore",        name: "Pale Ore",     img: "Pale_Ore.png",   min: 0, max: 6 },
     { key: "simpleKeys", name: "Simple Keys",  img: "Simple_Key.png", min: 0, max: 4 },
-    // PlayTime in save file is usually seconds (float)
     { key: "playTime",   name: "Play Time (s)", img: "Clock.png",     min: 0, max: 9999999, step: 100 },
 ];
 
@@ -27,7 +29,6 @@ export default function InventoryEditor({ data, onUpdate }) {
                         <img 
                             src={`icons/${item.img}`} 
                             alt={item.name}
-                            // If image fails (like Clock.png missing), hide image and show text only
                             onError={(e) => {e.target.style.display='none'}} 
                         />
                         <div className="charm-name">{item.name}</div>
@@ -42,12 +43,14 @@ export default function InventoryEditor({ data, onUpdate }) {
                                 onChange={(e) => handleChange(item.key, e.target.value)}
                             />
                         </div>
-                        
-                        {/* Helper for PlayTime to show hours */}
+
+                        {/* Helper for PlayTime */}
                         {item.key === "playTime" && (
-                            <div style={{fontSize: "0.7em", color: "#888", marginTop: "5px"}}>
-                                {(value / 3600).toFixed(1)} Hours
-                            </div>
+                            <div className="sub-text">{(value / 3600).toFixed(1)} Hours</div>
+                        )}
+                        {/* Helper for Soul */}
+                        {item.key === "MPReserveMax" && (
+                            <div className="sub-text">{(value / 33).toFixed(1)} Vessels</div>
                         )}
                     </div>
                 );
