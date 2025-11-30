36f1ac6a00f61cf50695d3caea311dbacb3c8646
dinglemire <enPaulius@gmail.com>
2025-11-30 19:41:53 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "docs"),
         filename: "bundle.js",
-        clean: true 
+        clean: false
     },
     module: {
         rules: [
